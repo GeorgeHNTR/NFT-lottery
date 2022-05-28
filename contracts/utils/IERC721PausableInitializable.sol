8b4a1a157a64111539f68ecc3da479789a642f05
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-05-28 18:34:31 +0300

    change interface to pausable

---
+++ contracts/utils/IERC721PausableInitializable.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC721PausableInitializable {
+    /**
+     * @dev Do not forget to add the "initializer" custom modifer
+     */
+    function initialize(string memory name_, string memory symbol_) external;
+}
