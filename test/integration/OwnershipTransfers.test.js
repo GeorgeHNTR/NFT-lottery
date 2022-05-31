1cf9ec3a7fa70b8b564750eeb815e619717f3580
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-05-31 05:30:19 +0300

    intergration test: ownership transfers

--- test/integration/OwnershipTransfers.test.js
+++ test/integration/OwnershipTransfers.test.js
@@ -4,9 +4,9 @@ const { ethers } = require("hardhat");
 let { RINKEBY__VRF_COORDINATOR, RINKEBY__LINK_TOKEN, RINKEBY__KEYHASH } = require('../utils/chainlink');
 let { NAME, SYMBOL, PRICE, getBlocks } = require("../utils/ticket");
 
-describe('DeploymentManagement (LotteryManager-TicketFactory)', async function () {
+describe.only('OwnershipTransfers (LotteryManager-TicketFactory-TicketBeacon)', async function () {
     beforeEach(async function () {
-        [deployer] = await ethers.getSigners();
+        [deployer, newOwner] = await ethers.getSigners();
         this.LotteryManager = await (await ethers.getContractFactory("LotteryManager")).deploy();
 
         this.TicketImplementationAddress = (await (await ethers.getContractFactory("Ticket")).deploy()).address;
@@ -15,19 +15,21 @@ describe('DeploymentManagement (LotteryManager-TicketFactory)', async function (
 
         await this.LotteryManager.setupLottery(this.TicketImplementationAddress, this.WinnerPickerAddress);
 
+        this.TicketBeacon = await ethers.getContractAt("TicketBeacon", await this.LotteryManager.ticketBeacon());
         this.TicketFactory = await ethers.getContractAt("TicketFactory", await this.LotteryManager.ticketFactory());
 
         this.BLOCKS = await getBlocks();
         this.PARAMS = [NAME, SYMBOL, this.BLOCKS.START_BLOCK, this.BLOCKS.END_BLOCK, PRICE];
     });
 
-    it("Lottery manager should be able to deploy new ticket proxies", async function () {
-        await this.LotteryManager.deployTicketProxy(...this.PARAMS);
-        expect(await this.TicketFactory.latestTicketProxy()).to.not.equal(ethers.constants.AddressZero);
+    it("Both factory and beacon should be owned by the lottery manager", async function () {
+        expect(await this.TicketFactory.owner()).to.equal(this.LotteryManager.address);
+        expect(await this.TicketBeacon.owner()).to.equal(this.LotteryManager.address);
     });
 
-    it("Lottery manager should not be able to deploy new ticket proxies if latest one has not finished", async function () {
-        await this.LotteryManager.deployTicketProxy(...this.PARAMS);
-        await expect(this.LotteryManager.deployTicketProxy(...this.PARAMS)).to.be.revertedWith("OnlyOneTicketAtTime()");
+    it("Both factory and beacon should change owner once the lottery manager decides to", async function () {
+        await this.LotteryManager.transferLotteryOwnership(newOwner.address);
+        expect(await this.TicketFactory.owner()).to.equal(newOwner.address);
+        expect(await this.TicketBeacon.owner()).to.equal(newOwner.address);
     });
 });
