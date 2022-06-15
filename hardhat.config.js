3ff78ad3e1654763c938cff3dcd75cfad1fa9b2c
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-06-15 23:26:26 +0300

    install gas reporter & coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require('hardhat-docgen');
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
   settings: {
     optimizer: {
       enabled: true,
       runs: 1000,
     },
   },
+  gasReporter: {
+    currency: 'USD',
+    // coinmarketcap: process.env.API_KEY__COINMARKETCAP
+  }
   // networks: {
   //   rinkeby: {
   //     url: `https://rinkeby.infura.io/v3/0dc62080d95a458fbcd7bd0e28a1a95d`,
